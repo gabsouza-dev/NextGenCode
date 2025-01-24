1f4a857f26fe439a5a861b7af7f379f84267bcfd
gabsouza-dev <dev.gbrlsouza@gmail.com>
2025-01-24 09:44:23 -0300

    Update Go.go

--- Go.go
+++ Go.go
@@ -1,25 +1,14 @@
 package main
-import "fmt"
-func main() {
-    fmt.Println("Hello, World!")
-}
 
-package main
 import (
     "fmt"
     "math/rand"
     "time"
 )
+
 func main() {
+    fmt.Println("Hello, World!")
     rand.Seed(time.Now().UnixNano())
     fmt.Println(rand.Intn(100) + 1)
-}
-
-package main
-import (
-    "fmt"
-    "time"
-)
-func main() {
     fmt.Println(time.Now())
 }
